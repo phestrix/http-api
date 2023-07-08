d91539a703cf5437adb11a1d957205b2c5a0513e
matri <matri@NITRO5>
2023-07-08 13:02:02 +0700

    added routes to customers

---
+++ src/main/kotlin/com/example/routes/CustomerRoutes.kt
@@ -0,0 +1,14 @@
+package com.example.routes
+
+import io.ktor.server.routing.*
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {}
+        get("/id?") {
+
+        }
+        post { }
+        delete("/id?") {}
+    }
+}
