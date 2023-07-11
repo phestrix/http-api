dd07d4d4029f0c3ca2d5f19cc7a9b440cf61eb7c
phestrix <matrixx.panasx@gmail.com>
2023-07-11 18:52:20 +0700

    Implemented email validation

--- src/main/kotlin/com/example/util/Validation.kt
+++ src/main/kotlin/com/example/util/Validation.kt
@@ -1,5 +1,10 @@
 package com.example.util
 
+import java.util.regex.Pattern
 
-//TODO need to implement
-fun String.isValidEmail(): Boolean = true
+
+fun String.isValidEmail(): Boolean {
+    val pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}\$")
+    val matcher = pattern.matcher(this)
+    return matcher.matches()
+}
