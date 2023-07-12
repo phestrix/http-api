9c1d376c50da9f8820992e65142f29366b51a7bc
phestrix <matrixx.panasx@gmail.com>
2023-07-12 23:40:22 +0700

    changed validation to regex from kotlin.text

--- src/main/kotlin/com/example/util/Validation.kt
+++ src/main/kotlin/com/example/util/Validation.kt
@@ -1,10 +1,7 @@
 package com.example.util
 
-import java.util.regex.Pattern
-
 
 fun String.isValidEmail(): Boolean {
-    val pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}\$")
-    val matcher = pattern.matcher(this)
-    return matcher.matches()
+    val regex = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
+    return regex.matches(this)
 }
